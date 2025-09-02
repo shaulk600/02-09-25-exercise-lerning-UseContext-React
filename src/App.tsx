8b806d293ebaa732d844c73443d2749375d57da4
Shaul Kimel <shaullk600@gmail.com>
2025-09-02 09:39:11 +0300

    create git: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import react from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/comps/Task1/Task1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Task1() {
+  return (
+    <div></div>
+  )
+}
